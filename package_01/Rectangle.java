887fc9e7c0e845269d1f0afb2ece3b6b1c7a510c
Vlad Smirnykh <vlad.ainur@gmail.com>
2021-04-01 23:33:01 +0300

    Add the first task of the 1st lesson of Java Essential

--- package_01/Rectangle.java
+++ package_01/Rectangle.java
@@ -1,5 +1,10 @@
 package package_01;
-
+// Создать класс с именем Rectangle. В теле класса
+//создать два поля, описывающие длины сторон double side1, double side2. Создать два метода,
+//вычисляющие площадь прямоугольника – double areaCalculator (double side1, double side2) и периметр
+//прямоугольника – double perimeterCalculator (double side1, double side2). Написать программу, которая
+//принимает от пользователя длины двух сторон прямоугольника и выводит на экран периметр и
+//площадь.
 
 public class Rectangle {
     double side1;
